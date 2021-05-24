8bba79335fc0f1e8ca5d6e988b36eb694de594b8
Densovan <sarimramata@gmail.com>
2021-05-25 02:16:44 +0700

    add more

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Row, Col } from "antd";
 import AuthContext from "../contexts/authContext";
 import TopNavbar from "../components/Layouts/topNavbar";
@@ -7,8 +7,17 @@ import Footer from "../components/Layouts/footer";
 import { useRouter } from "next/router";
 import { FaCamera } from "react-icons/fa";
 import Link from "next/link";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../graphql/query";
+import { AiOutlineAppstoreAdd, AiOutlineEdit } from "react-icons/ai";
 
 const Dashboard = () => {
+  const [change, setChange] = useState({
+    value: "all-story",
+  });
+
+  const { loading, data, error } = useQuery(GET_USER);
+  if (loading) return "";
   const router = useRouter();
   const { loggedIn } = useContext(AuthContext);
   return (
@@ -30,34 +39,97 @@ const Dashboard = () => {
                   {/* <div className="uplaod-mg-btn">
                       <FaCamera size={17} />
                     </div> */}
-                  <h1>Sovanden</h1>
+                  <h1>{data.get_user.fullname}</h1>
                 </center>
-                {/* <div>
-                  <h4>Your Stories</h4>
-                </div> */}
                 <div className="container-profile">
-                  <h4>Your Information</h4>
-                  <h4></h4>
+                  <h3>Your Information</h3>
+                  <div className="hold-info">
+                    <h4>Fullname</h4>
+                    <p>{data.get_user.fullname}</p>
+                  </div>
+
+                  <div className="hold-info">
+                    <h4>Email</h4>
+                    <p>{data.get_user.email}</p>
+                  </div>
+                  <h3>Managements</h3>
+                  <div className="list-content-pf">
+                    <div
+                      className={
+                        change.value === "all-story"
+                          ? "active-list-pf"
+                          : "a-list-content-pf"
+                      }
+                      onClick={() => setChange({ value: "all-story" })}
+                      // className="a-list-content-pf"
+                    >
+                      <span>
+                        {" "}
+                        <AiOutlineAppstoreAdd
+                          size={16}
+                          style={{ marginRight: "10px" }}
+                        />
+                        All Stories
+                      </span>
+                    </div>
+                    <div
+                      className={
+                        change.value === "add-story"
+                          ? "active-list-pf"
+                          : "a-list-content-pf"
+                      }
+                      onClick={() => setChange({ value: "add-story" })}
+                      // className="a-list-content-pf"
+                    >
+                      <span>
+                        {" "}
+                        <AiOutlineEdit
+                          size={16}
+                          style={{ marginRight: "10px" }}
+                        />
+                        Add Story
+                      </span>
+                    </div>
+                  </div>
                 </div>
                 <br></br>
               </div>
             </Col>
             <Col sm={24} lg={16}>
               <div className="profile-content">
-                <div className="sub-pf-content">
-                  <h2>Your Post</h2>
-                  <Row>
-                    <Col sm={24} md={8}>
-                      xzdf
-                    </Col>
-                    <Col sm={24} md={8}>
-                      xzdf
-                    </Col>
-                    <Col sm={24} md={8}>
-                      xzdf
-                    </Col>
-                  </Row>
-                </div>
+                {change.value === "all-story" && (
+                  <div className="sub-pf-content">
+                    <h2>Your Stories</h2>
+                    <Row>
+                      <Col sm={24} md={8}>
+                        xzdf
+                      </Col>
+                      <Col sm={24} md={8}>
+                        xzdf
+                      </Col>
+                      <Col sm={24} md={8}>
+                        xzdf
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+                {/* =============value = add-story=========== */}
+                {change.value === "add-story" && (
+                  <div className="sub-pf-content">
+                    <h2>Add Your Story</h2>
+                    <Row>
+                      <Col sm={24} md={8}>
+                        xzdf
+                      </Col>
+                      <Col sm={24} md={8}>
+                        xzdf
+                      </Col>
+                      <Col sm={24} md={8}>
+                        xzdf
+                      </Col>
+                    </Row>
+                  </div>
+                )}
               </div>
             </Col>
           </Row>
