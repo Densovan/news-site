7e9f00dd7d0561d486e698b7b108040af626ae32
Densovan <sarimramata@gmail.com>
2021-08-25 11:02:34 +0700

    add

--- client/pages/404.js
+++ client/pages/404.js
@@ -9,7 +9,7 @@ const NotFound = () => {
     <React.Fragment>
       {/* <TopNavbar /> */}
       {/* <MainNavbar /> */}
-      <GlobalHeader />
+      {/* <GlobalHeader /> */}
       <div className="main-404">
         <div id="notfound">
           <div className="notfound">
