e56b47f7aa1d249974d8a67226b740a53db0a91b
vann-soklay <soklayvann@gmail.com>
2021-10-14 11:20:26 +0700

    change-loop-notification

--- client/components/common/notification.js
+++ client/components/common/notification.js
@@ -45,234 +45,7 @@ const Notification = ({ user }) => {
     for (let j = 0; j < cube.length; j++){  
         if (cube[j].user.fullname !== user.get_user.fullname) {
             sum += cube[j].count;
-            if (cube[j].type === 'follow') {
-                data.push(
-                    <div className="container-box" style={cube[j].read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
-                        <div className="box-notification">
-                          <div className="avatar-notification">
-                            <div><CaretRightOutlined /></div>
-                            <div style={{ paddingRight: 8 }}>
-                              <Avatar
-                                src={cube[j].user.image}
-                                size={40}
-                              />
-                            </div>
-                          </div>
-                          <div className="text-notification">
-                            <div style={{ paddingRight: 8 }}>
-                              <li>
-                                <strong>
-                                    {cube[j].user.fullname}{" "}
-                                </strong>{" "}
-                                have following on you.
-                              </li>
-                              <li style={{ paddingTop: 4, fontSize:13, color: '#38a7c8' }}>
-                                  <strong>{
-                                    timeAgo(cube[j].createdAt)
-                                  }
-                                  </strong>
-                              </li>
-                            </div>
-                          </div>
-                        </div>
-                    </div>
-                )
-            }
-            if (cube[j].type === 'news'){
-                data.push(
-                    <div className="container-box" style={cube[j].read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
-                        <div className="box-notification">
-                            <div className="avatar-notification">
-                              <div><CaretRightOutlined /></div>
-                              <div style={{ paddingRight: 8 }}>
-                                <Avatar
-                                  src={cube[j].user.image}
-                                  size={40}
-                                />
-                              </div>
-                            </div>
-                            <div className="text-notification">
-                              <div style={{ paddingRight: 8 }}>
-                                <li>
-                                  <strong>
-                                      {cube[j].user.fullname}{" "}
-                                  </strong>{" "}
-                                  have post: {" "}
-                                  {
-                                    cube[j].news.title.length <= 70
-                                    ? cube[j].news.title
-                                    : cube[j].news.title.substring(0, 70) + "..."
-                                  }
-                                </li>
-                                <li style={{ paddingTop: 4, fontSize:13, color: '#38a7c8' }}>
-                                  <strong>{
-                                    timeAgo(cube[j].createdAt)
-                                  }
-                                  </strong>
-                                </li>
-                              </div>
-                              <div className="image-container">
-                                <img src={`${URL_ACCESS}/public/uploads/${cube[j].news.thumnail}`}/>
-                              </div>
-                            </div>
-                        </div>
-                    </div>
-                  );
-            }
-            if (cube[j].type === 'comment') {
-                data.push(
-                  <div className="container-box" style={cube[j].read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
-                      <div className="box-notification">
-                          <div className="avatar-notification">
-                              <div><CaretRightOutlined /></div>
-                              <div style={{ paddingRight: 8 }}>
-                                <Avatar
-                                  src={cube[j].user.image}
-                                  size={40}
-                                />
-                              </div>
-                          </div>
-                          <div className="text-notification">
-                              <div style={{ paddingRight: 8 }}>
-                                <li>
-                                  <strong>
-                                      {cube[j].user.fullname}{" "}
-                                  </strong>{" "}
-                                  comment on your post.{" "}
-                                  {
-                                    cube[j].news.title.length <= 70
-                                    ? cube[j].news.title
-                                    : cube[j].news.title.substring(0, 70) + "..."
-                                  }
-                                </li>
-                                <li style={{ paddingTop: 4, fontSize:13, color: '#38a7c8' }}>
-                                  <strong>{
-                                    timeAgo(cube[j].createdAt)
-                                  }
-                                  </strong>
-                                </li>
-                              </div>
-                              <div className="image-container">
-                                <img src={`${URL_ACCESS}/public/uploads/${cube[j].news.thumnail}`}/>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                );
-            }
-            if (cube[j].type === 'reply') {
-                if (cube[j].userTo.fullname === user.get_user.fullname) {
-                  data.push(
-                      <div className="container-box" style={cube[j].read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
-                          <div className="box-notification">
-                            <div style={{ display: "flex", alignItems: "center"}}>
-                              <div><CaretRightOutlined /></div>
-                              <div style={{ paddingRight: 8 }}>
-                                <Avatar
-                                  src={cube[j].user.image}
-                                  size={40}
-                                />
-                              </div>
-                            </div>
-                              <div>
-                                  <strong>
-                                      {cube[j].user.fullname}{" "}
-                                  </strong>{" "}
-                                  mentioned you in a comment.{" "}
-                                  {
-                                    cube[j].news.title.length <= 70
-                                    ? cube[j].news.title
-                                    : cube[j].news.title.substring(0, 70) + "..."
-                                  }
-                              </div>
-                          </div>
-                      </div>
-                  );
-                }else{
-                data.push(
-                    <div className="container-box" style={cube[j].read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
-                        <div className="box-notification">
-                            <div style={{ display: "flex", alignItems: "center"}}>
-                              <div><CaretRightOutlined /></div>
-                              <div style={{ paddingRight: 8 }}>
-                                <Avatar
-                                  src={cube[j].user.image}
-                                  size={40}
-                                />
-                              </div>
-                            </div>
-                            <div className="text-notification">
-                              <div style={{ paddingRight: 8 }}>
-                                <li>
-                                  <strong>
-                                      {cube[j].user.fullname}{" "}
-                                  </strong>{" "}
-                                  replied to{' '}
-                                  <strong>
-                                      {cube[j].userTo.fullname}{" "}
-                                  </strong>{" "}
-                                  on post: {' '}
-                                  {
-                                    cube[j].news.title.length <= 70
-                                    ? cube[j].news.title
-                                    : cube[j].news.title.substring(0, 70) + "..."
-                                  }
-                                </li>
-                                <li style={{ paddingTop: 4, fontSize:13, color: '#38a7c8' }}>
-                                  <strong>{
-                                  timeAgo(cube[j].createdAt)
-                                  }</strong>
-                                </li>
-                              </div>
-                              <div className="image-container">
-                                <img src={`${URL_ACCESS}/public/uploads/${cube[j].news.thumnail}`}/>
-                              </div>
-                            </div>
-                        </div>
-                    </div>
-                );
-                }
-            }
-            if (cube[j].type === 'up') {
-                data.push(
-                    <div className="container-box" style={cube[j].read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
-                        <div className="box-notification">
-                            <div className="avatar-notification">
-                              <div><CaretRightOutlined /></div>
-                              <div style={{ paddingRight: 8 }}>
-                                <Avatar
-                                  src={cube[j].user.image}
-                                  size={40}
-                                />
-                              </div>
-                            </div>
-                            <div className="text-notification">
-                              <div style={{ paddingRight: 8 }}>
-                                <li>
-                                  <strong>
-                                      {cube[j].user.fullname}{" "}
-                                  </strong>{" "}
-                                  like on your post: {' '}
-                                  {
-                                    cube[j].news.title.length <= 65
-                                    ? cube[j].news.title
-                                    : cube[j].news.title.substring(0, 65) + "..."
-                                  }
-                                </li>
-                                <li style={{ paddingTop: 4, fontSize:13, color: '#38a7c8' }}>
-                                  <strong>{
-                                  timeAgo(cube[j].createdAt)
-                                  }</strong>
-                                </li>
-                              </div>
-                              <div className="image-container">
-                                <img src={`${URL_ACCESS}/public/uploads/${cube[j].news.thumnail}`}/>
-                              </div>
-                            </div>
-                        </div>
-                    </div>
-                );
-            }
+            data.push(cube[j])
         }
     }
   }
@@ -294,7 +67,56 @@ const Notification = ({ user }) => {
                   </Col>
                 </Row>
                 <Row>
-                    {data}
+                    {data.map(item => {
+                      return(
+                        <div className="container-box" style={item.read ? {backgroundColor:"white"} : {backgroundColor: "transparent"}}>
+                          <div className="box-notification">
+                              <div className="avatar-notification">
+                                <div><CaretRightOutlined /></div>
+                                <div style={{ paddingRight: 8 }}>
+                                  <Avatar
+                                    src={item.user.image}
+                                    size={40}
+                                  />
+                                </div>
+                              </div>
+                              <div className="text-notification">
+                                <div style={{ paddingRight: 8 }}>
+                                  <li>
+                                    <strong>
+                                      {item.user.fullname}
+                                    </strong>{" "}
+                                    { item.type === "up" && "like on your post: "}
+                                    { item.type === "comment" && "comment on your post: "}
+                                    { item.type === "reply" && (item.userTo.fullname == user.get_user.fullname ? "mentioned you in a comment: " : <>replied to <strong>{item.userTo.fullname}</strong> on your post: </>)}  
+                                    { item.type === "news" && " have post: "}
+                                    { item.type === "follow" && "have following on you. "}
+                                    { item.type !== "follow" ? 
+                                        item.news.title.length <= 65
+                                        ? item.news.title
+                                        : item.news.title.substring(0, 65) + "..."
+                                      :
+                                        ''
+                                    } 
+                                  </li>
+                                  <li style={{ paddingTop: 4, fontSize:13, color: '#38a7c8' }}>
+                                    <strong>{
+                                    timeAgo(item.createdAt)
+                                    }</strong>
+                                  </li>
+                                </div>
+                                { item.type !== "follow" ? 
+                                    <div className="image-container">
+                                      <img src={`${URL_ACCESS}/public/uploads/${item.news.thumnail}`}/>
+                                    </div>
+                                  :
+                                    ''
+                                } 
+                              </div>
+                          </div>
+                        </div>
+                      )
+                    })}
                 </Row>
               </div>
             </div>
